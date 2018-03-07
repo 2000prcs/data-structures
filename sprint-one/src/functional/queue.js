46e38639d1f881bd050a376d838510d3f8779441
Moyeong Lee <2000prcs@gmail.com>
2018-03-07 12:42:52 -0800

    Complete queue part
    - Completed queue shared behavior
    - Completed queue-specific behavior

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numberOfKeys = 0;
+  var dequeued = [];
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[numberOfKeys] = value;
+    numberOfKeys ++;
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    dequeued.push(storage[keys[0]]);
+    delete storage[keys[0]];
+    return dequeued[dequeued.length-1];
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
