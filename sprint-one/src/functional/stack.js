166665d042fb13fa0d138a5b68d9405768a1ebc2
Moyeong Lee <2000prcs@gmail.com>
2018-03-07 12:04:17 -0800

    Complete Stack part (stack shared behavior)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,30 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numberOfkey = 0;
+  var poped = [];
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[numberOfkey] = value;
+    numberOfkey++;
   };
 
   someInstance.pop = function() {
+    // how many keys in the storage
+    var keys = Object.keys(storage);
+    // store the last value to the poped array 
+    poped.push(storage[keys.length - 1]);
+    // remove the last value from the storage object
+    delete storage[keys.length - 1];
+    numberOfkey--;
+    //console.log('poped after delete', poped);
+    // return the last element of popled array
+    return poped[poped.length - 1];
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
