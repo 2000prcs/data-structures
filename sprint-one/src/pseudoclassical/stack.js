94ff4522c19f1040e3aab57411678bf632b4a1d5
Moyeong Lee <2000prcs@gmail.com>
2018-03-07 17:32:28 -0800

    Complete pseudoclassical pattern (stack part)

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keyCounter = 0;
+  this.popped = [];
 };
+
+Stack.prototype.size = function(){
+  return Object.keys(this.storage).length;
+}
+
+Stack.prototype.push = function(value){
+  this.storage[this.keyCounter] = value;
+  this.keyCounter ++;
+}
+
+Stack.prototype.pop = function(){
+  var keys = Object.keys(this.storage);
+  this.popped.push(this.storage[keys.length - 1]);
+  delete this.storage[keys.length - 1];
+  this.keyCounter --;
+  return this.popped[this.popped.length - 1];
+}
+//new keyword also returns automatically
+var NewInstance = new Stack();
 
 
