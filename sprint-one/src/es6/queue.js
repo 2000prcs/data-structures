17da0987cc970ce4456da541c76c6b8683dd290b
Moyeong Lee <2000prcs@gmail.com>
2018-03-07 17:48:52 -0800

    Complete ES6 pattern (queue part)

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.keyCounter = 0;
+    this.dequeued = [];
   }
 
+  size(){
+    return Object.keys(this.storage).length;
+  }
+
+  enqueue(value){
+    this.storage[this.keyCounter] = value;
+    this.keyCounter ++;
+  }
+
+  dequeue(){
+    var keys = Object.keys(this.storage);
+    this.dequeued.push(this.storage[keys[0]]);
+    delete this.storage[keys[0]];
+    return this.dequeued[this.dequeued.length - 1];
+  }
 }
+
+var newInstance = new Queue();
