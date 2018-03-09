be25b148266436241c7fc62827e8039db13f9441
2000prcs <2000prcs@gmail.com>
2018-03-09 13:13:30 -0800

    Complete graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,10 +7,8 @@ var Graph = function() {
 
 //Add Graph properties
 
-
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  //debugger;
   var nodeString = "" + node;
   var nodeObject = {}; 
   nodeObject.value = node;
@@ -39,25 +37,14 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  console.log('addEdge', this);
   this["" + fromNode].edge = [];
   this["" + toNode].edge = [];
-  //console.log(this);
-  //console.log(this["" + fromNode].edge);
   this["" + fromNode].edge.push(toNode);
   this["" + toNode].edge.push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-//   this["" + fromNode].edge = this["" + fromNode].edge.filter(function(val) {
-//     val !== toNode
-//   });
-//   this["" + toNode].edge = this["" + toNode].edge.filter(function(val) {
-//     val !== fromNode
-//   });
-// console.log(this["" + fromNode].edge);
-// console.log(this["" + toNode].edge);
   delete this["" + fromNode].edge;
   delete this["" + toNode].edge; 
 };
@@ -65,16 +52,11 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   // iterate through object to invoke callback per each node
-
-  // console.log(this);
-  // var result;
-  // function closure(){
-  //   console.log(cb);
-  //   result = cb.apply(this, arguments);
-  //   var args = [...arguments]
-  //   console.log(args);
-  // }
-  // closure();
+  for(var key in this) {
+    if(typeof this[key] !== "function") {
+      cb.call(this, key);
+    }
+  }
 };
 
 /*
