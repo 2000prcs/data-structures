a2a9b53bcec90defcf10de6300e43aef3d43d33d
2000prcs <2000prcs@gmail.com>
2018-03-08 18:26:35 -0800

    Completed part of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,26 +2,39 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  
 };
+
+//Add Graph properties
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.node = node;
+  // console.log(JSONstringify(node));
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.node === node;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.node;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var edgeKey = String(fromNode + "," + toNode);
+  return !!this[edgeKey]; 
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // fromNode[] = toNode[value];
+  var newEdgeKey = String(fromNode + "," + toNode);
+  this[newEdgeKey] = [fromNode, toNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -35,5 +48,4 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
