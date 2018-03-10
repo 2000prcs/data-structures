e524ba19062a2daa22f5aebe324add4f6960711a
2000prcs <2000prcs@gmail.com>
2018-03-10 11:25:47 -0800

    Complete advanced part of hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,7 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._totalItems = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -12,17 +13,36 @@ HashTable.prototype.insert = function(k, v) {
     } else {
     this._storage[index].push([k, v]);
     }
-
+  this._totalItems += 1;
+  //check totalItems/limit
+  //call resize func
+  if((this._totalItems / this._limit) > 0.75) {
+    this._limit = this._limit*2;
+    var oldStorage = this._storage;
+    this._totalItems = 0;
+    this._storage = LimitedArray(this._limit);
+    for(var key in oldStorage){
+      if(typeof oldStorage[key] !== 'function'){
+        for(var i= 0; i< oldStorage[key].length; i++) {
+          this.insert(...oldStorage[key][i]); //...[k, v]
+        }
+      }
+    }
+  } 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var res;
-  for (var i=0; i<this._storage[index].length; i++){
-    if(this._storage[index][i][0] === k) {
-      res = this._storage[index][i][1];
-      }
+  for(var key in this._storage) {
+    if(typeof this._storage[key] !== "function") {
+      this._storage[key].forEach( function(val) {
+        if(val[0] === k){
+          res = val[1];
+        }
+      })
     }
+  }
   return res;
 };
 
@@ -34,6 +54,20 @@ HashTable.prototype.remove = function(k) {
       this._storage[index].splice(i, 1);
     }
   }
+  this._totalItems -= 1;
+  if((this._totalItems / this._limit) < 0.25) {
+    this._limit = this._limit / 2;
+    var oldStorage = this._storage;
+    this._totalItems = 0;
+    this._storage = LimitedArray(this._limit);
+    for(var key in oldStorage){
+      if(typeof oldStorage[key] !== 'function'){
+        for(var i= 0; i< oldStorage[key].length; i++) {
+          this.insert(...oldStorage[key][i]); //...[k, v]
+        }
+      }
+    }
+  } 
 };
 
 
