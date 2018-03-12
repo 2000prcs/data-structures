cdea8c816f4b255fb12ddb0e37c634cebcf2aafe
Moyeong Lee <2000prcs@gmail.com>
2018-03-11 19:15:59 -0700

    Refactor/Clean-up Sprint 1 (functional)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,28 +1,26 @@
 var Queue = function() {
-  var someInstance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
   var numberOfKeys = 0;
   var dequeued = [];
 
-  // Implement the methods below
-
-  someInstance.enqueue = function(value) {
+  instance.enqueue = function(value) {
     storage[numberOfKeys] = value;
     numberOfKeys ++;
   };
 
-  someInstance.dequeue = function() {
+  instance.dequeue = function() {
     var keys = Object.keys(storage);
     dequeued.push(storage[keys[0]]);
     delete storage[keys[0]];
     return dequeued[dequeued.length-1];
   };
 
-  someInstance.size = function() {
+  instance.size = function() {
     return Object.keys(storage).length;
   };
 
-  return someInstance;
+  return instance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,33 +1,31 @@
 var Stack = function() {
-  var someInstance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var numberOfkey = 0;
-  var poped = [];
+  var numberOfKey = 0;
+  var popped = [];
 
-  // Implement the methods below
-  someInstance.push = function(value) {
-    storage[numberOfkey] = value;
-    numberOfkey++;
+  instance.push = function(value) {
+    storage[numberOfKey] = value;
+    numberOfKey++;
   };
 
-  someInstance.pop = function() {
+  instance.pop = function() {
     // how many keys in the storage
     var keys = Object.keys(storage);
-    // store the last value to the poped array 
-    poped.push(storage[keys.length - 1]);
+    // store the last value to the popped array 
+    popped.push(storage[keys.length - 1]);
     // remove the last value from the storage object
     delete storage[keys.length - 1];
-    numberOfkey--;
-    //console.log('poped after delete', poped);
+    numberOfKey--;
     // return the last element of popled array
-    return poped[poped.length - 1];
+    return popped[poped.length - 1];
   };
 
-  someInstance.size = function() {
+  instance.size = function() {
     return Object.keys(storage).length;
   };
 
-  return someInstance;
+  return instance;
 };
